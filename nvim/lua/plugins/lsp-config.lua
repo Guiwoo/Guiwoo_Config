fba0d217d3c551d5f1d15c683fe42a5c56515d52
Guiwoo Park <park.guiwoo@hotmail.com>
2025-07-08 01:25:26 +0900

    nvim lsp setting

---
+++ nvim/lua/plugins/lsp-config.lua
@@ -0,0 +1,44 @@
+return {
+  {
+    "mason-org/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end
+  },
+  {
+    "mason-org/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls","gopls" }
+      })
+    end
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local lspconfig = require("lspconfig")
+      lspconfig.lua_ls.setup({})
+      local lspconfig = require("lspconfig")
+      lspconfig.gopls.setup({})
+
+      vim.diagnostic.config({
+        virtual_text = true,  -- 줄 옆에 메시지 표시
+        signs = true,         -- gutter 에 E, W 아이콘 표시
+        underline = true,     -- 에러 줄 밑줄
+        update_in_insert = false,
+        severity_sort = true,
+        float = {
+          border = "rounded",
+          source = "always",
+          header = "",
+          prefix = "",
+        },
+      })
+
+      vim.keymap.set('n','K',vim.lsp.buf.hover,{})
+      vim.keymap.set('n','gd',vim.lsp.buf.definition,{})
+      vim.keymap.set({'n'}, '<leader>ca', vim.lsp.buf.code_action,{})
+    end
+  }
+}
+
