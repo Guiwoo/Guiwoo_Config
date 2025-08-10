b4338d3e089654a405e85436f50cafc10f900ec5
Guiwoo Park <park.guiwoo@hotmail.com>
2025-08-10 23:21:49 +0900

    nvim added go back go forward

--- nvim/lua/vim-keymap.lua
+++ nvim/lua/vim-keymap.lua
@@ -1,2 +1,6 @@
-vim.keymap.set("n","gl", function() vim.diagnostic.open_float() end, {desc="Open diagnostic in Float"}
-)
+vim.keymap.set("n", "gl", function()
+	vim.diagnostic.open_float()
+end, { desc = "Open diagnostic in Float" })
+
+vim.keymap.set("n", "<leader>grb", "<C-o>", { desc = "Go Back" })
+vim.keymap.set("n", "<leader>grf", "<C-i>", { desc = "Go Forward" }) -- Tab과 충돌 시 키 바꾸세요
