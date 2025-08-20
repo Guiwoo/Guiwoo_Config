79827a705b141e9205bf7dd3e13ae243d994a048
guiwoo <guiwoo.park@vueron.com>
2025-08-20 11:27:40 +0900

    nvimlua keymap added

--- nvim/lua/plugins/git-signs.lua
+++ nvim/lua/plugins/git-signs.lua
@@ -1,4 +1,7 @@
 return {
   'lewis6991/gitsigns.nvim',
   opts={},
+  keys={
+    {"<leader>tgc",":Gitsigns toggle_current_line_blame<CR>",mode="n",desc="Gitsigns toggle blame"},
+  }
 }
--- nvim/lua/vim-keymap.lua
+++ nvim/lua/vim-keymap.lua
@@ -2,5 +2,5 @@ vim.keymap.set("n", "gl", function()
 	vim.diagnostic.open_float()
 end, { desc = "Open diagnostic in Float" })
 
-vim.keymap.set("n", "<leader>grb", "<C-o>", { desc = "Go Back" })
-vim.keymap.set("n", "<leader>grf", "<C-i>", { desc = "Go Forward" }) -- Tab과 충돌 시 키 바꾸세요
+vim.keymap.set("n", "grb", "<C-o>", { desc = "Go Back" })
+vim.keymap.set("n", "grf", "<C-i>", { desc = "Go Forward" }) -- Tab과 충돌 시 키 바꾸세요
