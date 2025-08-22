2e8096df24a6d91e23f259cfd4ab9a5e19480dd9
guiwoo <guiwoo.park@vueron.com>
2025-08-22 16:27:15 +0900

    ghossty font, theme added

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -1,19 +1,31 @@
 return {
-  "rebelot/kanagawa.nvim",
-  config = function()
-    require("kanagawa").setup({
-      compile = true,
-      transparent = true,
-      theme = "wave",
-      background = {
-        dark = "wave",
-        light = "lotus",
-      },
-    });
-    vim.cmd("colorscheme kanagawa");
-    vim.api.nvim_set_hl(0, "Visual", { bg = "#f5e0b8", fg = "#3c3836" });
-  end,
-  build = function()
-    vim.cmd("KanagawaCompile");
-  end
+	{
+		"rebelot/kanagawa.nvim",
+		config = function()
+			require("kanagawa").setup({
+				compile = true,
+				transparent = true,
+				theme = "wave",
+				background = {
+					dark = "wave",
+					light = "lotus",
+				},
+			})
+			vim.cmd("colorscheme kanagawa")
+			vim.api.nvim_set_hl(0, "Visual", { bg = "#f5e0b8", fg = "#3c3836" })
+		end,
+		build = function()
+			vim.cmd("KanagawaCompile")
+		end,
+	},
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+	},
+  {
+    "olimorris/onedarkpro.nvim",
+    name = "onedark",
+    priority = 999,
+  }
 }
