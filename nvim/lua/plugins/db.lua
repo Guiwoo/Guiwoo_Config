bda5c0d7908136771d6dc119a7310d5e997ef05a
Guiwoo Park <park.guiwoo@hotmail.com>
2026-02-01 22:29:44 +0900

    trouble lua added

--- nvim/lua/plugins/db.lua
+++ nvim/lua/plugins/db.lua
@@ -1,5 +1,17 @@
 -- Database Plugins
 -- DB 관련 플러그인
 return {
-
+	{
+		"tpope/vim-dadbod",
+	},
+	{
+		"kristijanhusak/vim-dadbod-ui",
+		dependencies = { "tpope/vim-dadbod" },
+		cmd = { "DBUI", "DBUIToggle" },
+	},
+	{
+		"kristijanhusak/vim-dadbod-completion",
+		dependencies = { "tpope/vim-dadbod" },
+		ft = { "sql", "mysql", "plsql" },
+	},
 }
--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -2,9 +2,6 @@ return {
 	-- Main LSP Configuration
 	"neovim/nvim-lspconfig",
 	dependencies = {
-		-- Automatically install LSPs and related tools to stdpath for Neovim
-		-- Mason must be loaded before its dependents so we need to set it up here.
-		-- NOTE: `opts = {}` is the same as calling `require('mason').setup({})`
 		{ "mason-org/mason.nvim", opts = {} },
 		"mason-org/mason-lspconfig.nvim",
 		"WhoIsSethDaniel/mason-tool-installer.nvim",
@@ -16,53 +13,17 @@ return {
 	},
 	config = function()
 		local has_fzf, fzf_lua = pcall(require, "fzf-lua")
-		-- Brief aside: **What is LSP?**
-		--
-		-- LSP is an initialism you've probably heard, but might not understand what it is.
-		--
-		-- LSP stands for Language Server Protocol. It's a protocol that helps editors
-		-- and language tooling communicate in a standardized fashion.
-		--
-		-- In general, you have a "server" which is some tool built to understand a particular
-		-- language (such as `gopls`, `lua_ls`, `rust_analyzer`, etc.). These Language Servers
-		-- (sometimes called LSP servers, but that's kind of like ATM Machine) are standalone
-		-- processes that communicate with some "client" - in this case, Neovim!
-		--
-		-- LSP provides Neovim with features like:
-		--  - Go to definition
-		--  - Find references
-		--  - Autocompletion
-		--  - Symbol Search
-		--  - and more!
-		--
-		-- Thus, Language Servers are external tools that must be installed separately from
-		-- Neovim. This is where `mason` and related plugins come into play.
-		--
-		-- If you're wondering about lsp vs treesitter, you can check out the wonderfully
-		-- and elegantly composed help section, `:help lsp-vs-treesitter`
-
-		--  This function gets run when an LSP attaches to a particular buffer.
-		--    That is to say, every time a new file is opened that is associated with
-		--    an lsp (for example, opening `main.rs` is associated with `rust_analyzer`) this
-		--    function will be executed to configure the current bufferfzf
 		vim.api.nvim_create_autocmd("LspAttach", {
 			group = vim.api.nvim_create_augroup("kickstart-lsp-attach", { clear = true }),
 			callback = function(event)
-
 				local map = function(keys, func, desc, mode)
 					mode = mode or "n"
 					vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
 				end
 
-				-- Rename the variable under your cursor.
-				--  Most Language Servers support renaming across files, etc.
 				map("grn", vim.lsp.buf.rename, "[R]e[n]ame")
-
-				-- Execute a code action, usually your cursor needs to be on top of an error
-				-- or a suggestion from your LSP for this to activate.
 				map("gra", vim.lsp.buf.code_action, "[G]oto Code [A]ction", { "n", "x" })
 
-				-- Find references for the word under your cursor.
 				map("grr", has_fzf and fzf_lua.lsp_references or vim.lsp.buf.references, "[G]oto [R]eferences")
 				map("grb", function()
 					vim.cmd("normal! <C-o>")
@@ -71,31 +32,33 @@ return {
 					vim.cmd("normal! <C-o>")
 				end, "[G]o [f]orward previous position")
 
-				-- Jump to the implementation of the word under your cursor.
-				--  Useful when your language has ways of declaring types without an actual implementation.
-				map("gri", has_fzf and fzf_lua.lsp_implementations or vim.lsp.buf.implementation, "[G]oto [I]mplementation")
+				map(
+					"gri",
+					has_fzf and fzf_lua.lsp_implementations or vim.lsp.buf.implementation,
+					"[G]oto [I]mplementation"
+				)
 
-				-- Jump to the definition of the word under your cursor.
-				--  This is where a variable was first declared, or where a function is defined, etc.
-				--  To jump back, press <C-t>.
 				map("grd", has_fzf and fzf_lua.lsp_definitions or vim.lsp.buf.definition, "[G]oto [D]efinition")
 
-				-- WARN: This is not Goto Definition, this is Goto Declaration.
-				--  For example, in C this would take you to the header.
 				map("grD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
 
-				-- Fuzzy find all the symbols in your current document.
-				--  Symbols are things like variables, functions, types, etc.
-				map("gO", has_fzf and fzf_lua.lsp_document_symbols or vim.lsp.buf.document_symbol, "Open Document Symbols")
+				map(
+					"gO",
+					has_fzf and fzf_lua.lsp_document_symbols or vim.lsp.buf.document_symbol,
+					"Open Document Symbols"
+				)
 
-				-- Fuzzy find all the symbols in your current workspace.
-				--  Similar to document symbols, except searches over your entire project.
-				map("gW", has_fzf and fzf_lua.lsp_dynamic_workspace_symbols or vim.lsp.buf.workspace_symbol, "Open Workspace Symbols")
+				map(
+					"gW",
+					has_fzf and fzf_lua.lsp_dynamic_workspace_symbols or vim.lsp.buf.workspace_symbol,
+					"Open Workspace Symbols"
+				)
 
-				-- Jump to the type of the word under your cursor.
-				--  Useful when you're not sure what type a variable is and you want to see
-				--  the definition of its *type*, not where it was *defined*.
-				map("grt", has_fzf and fzf_lua.lsp_type_definitions or vim.lsp.buf.type_definition, "[G]oto [T]ype Definition")
+				map(
+					"grt",
+					has_fzf and fzf_lua.lsp_type_definitions or vim.lsp.buf.type_definition,
+					"[G]oto [T]ype Definition"
+				)
 
 				-- This function resolves a difference between neovim nightly (version 0.11) and stable (version 0.10)
 				---@param client vim.lsp.Client
@@ -110,11 +73,6 @@ return {
 					end
 				end
 
-				-- The following two autocommands are used to highlight references of the
-				-- word under your cursor when your cursor rests there for a little while.
-				--    See `:help CursorHold` for information about when this is executed
-				--
-				-- When you move your cursor, the highlights will be cleared (the second autocommand).
 				local client = vim.lsp.get_client_by_id(event.data.client_id)
 				if
 					client
@@ -221,12 +179,6 @@ return {
 			-- rust_analyzer = {},
 			-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
 			--
-			-- Some languages (like typescript) have entire language plugins that can be useful:
-			--    https://github.com/pmizio/typescript-tools.nvim
-			--
-			-- But for many setups, the LSP (`ts_ls`) will work just fine
-			-- ts_ls = {},
-			--
 			lua_ls = {
 				diagnostics = {
 					globals = { "vim" },
@@ -234,19 +186,6 @@ return {
 			},
 		}
 
-		-- Ensure the servers and tools above are installed
-		--
-		-- To check the current status of installed tools and/or manually install
-		-- other tools, you can run
-		--    :Mason
-		--
-		-- You can press `g?` for help in this menu.
-		--
-		-- `mason` had to be setup earlier: to configure its options see the
-		-- `dependencies` table for `nvim-lspconfig` above.
-		--
-		-- You can add other tools here that you want Mason to install
-		-- for you, so that they are available from within Neovim.
 		local ensure_installed = vim.tbl_keys(servers or {})
 		vim.list_extend(ensure_installed, {
 			"stylua", -- Used to format Lua code
